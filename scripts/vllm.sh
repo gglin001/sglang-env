6748e07ca5918d8f1a3df2a22e4f9f190a2e4873
Allen Guo <allenguo@vastaitech.com>
2026-03-02 11:46:14 +0000

    cu129

--- scripts/vllm.sh
+++ scripts/vllm.sh
@@ -1,4 +1,6 @@
 # cu130 or cu129 ?
 curl -L -O https://github.com/vllm-project/vllm/releases/download/v0.16.0/vllm-0.16.0+cu130-cp38-abi3-manylinux_2_35_x86_64.whl
-
 uv pip install --system vllm-0.16.0+cu130-cp38-abi3-manylinux_2_35_x86_64.whl
+
+curl -L -O https://github.com/vllm-project/vllm/releases/download/v0.16.0/vllm-0.16.0-cp38-abi3-manylinux_2_31_x86_64.whl
+uv pip install --system vllm-0.16.0-cp38-abi3-manylinux_2_31_x86_64.whl
