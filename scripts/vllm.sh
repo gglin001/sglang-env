7ee7a0b2420388e971cc7c53999209f37850c155
Allen Guo <guosonglin001@gmail.com>
2026-03-03 05:49:41 +0000

    optional

--- scripts/vllm.sh
+++ scripts/vllm.sh
@@ -7,3 +7,6 @@ uv pip install --system vllm-0.16.0+cu130-cp38-abi3-manylinux_2_35_x86_64.whl
 
 curl -L -O https://github.com/vllm-project/vllm/releases/download/v0.16.0/vllm-0.16.0-cp38-abi3-manylinux_2_31_x86_64.whl
 uv pip install --system vllm-0.16.0-cp38-abi3-manylinux_2_31_x86_64.whl
+
+# optional
+# uv pip install --system "transformers>=5.0.0"
