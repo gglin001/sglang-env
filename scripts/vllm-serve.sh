4b3b9e905ec9c89b0348030a250e596f5d93970e
Allen Guo <guosonglin001@gmail.com>
2026-06-07 16:01:00 +0800

    no default

--- scripts/vllm-serve.sh
+++ scripts/vllm-serve.sh
@@ -9,17 +9,22 @@ export CUDA_DEVICE_ORDER=PCI_BUS_ID
 # pixi shell -e cu130
 # pixi shell -e cu129
 
+export VLLM_NO_USAGE_STATS=1
+export VLLM_DO_NOT_TRACK=1
+export DO_NOT_TRACK=1
+
 args=(
   #
-  third_party/GLM-OCR
-  --served-model-name OCR
-  --speculative-config.method mtp
-  --speculative-config.num_speculative_tokens 1
-  #
-  # third_party/FireRed-OCR
+  # third_party/GLM-OCR
   # --served-model-name OCR
+  # --speculative-config.method mtp
+  # --speculative-config.num_speculative_tokens 1
+  #
+  third_party/FireRed-OCR
+  --served-model-name OCR
   #
   # --load-format dummy
+  # --enforce-eager
   #
   # --host 0.0.0.0
   --host 127.0.0.1
@@ -27,6 +32,7 @@ args=(
   #
   --trust-remote-code
   --uvicorn-log-level warning
+  # --disable-log-requests
   #
   # -tp 2
   #
