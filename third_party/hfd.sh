9ef788e8b7a1b414664f958e277451ae6a902d50
Allen Guo <guosonglin001@gmail.com>
2026-06-07 17:43:32 +0800

    datalab-to/chandra-ocr-2

--- third_party/hfd.sh
+++ third_party/hfd.sh
@@ -13,4 +13,7 @@ hfd.sh zai-org/GLM-OCR
 hfd.sh FireRedTeam/FireRed-OCR
 # https://huggingface.co/FireRedTeam/FireRed-OCR
 
+hfd.sh datalab-to/chandra-ocr-2
+# https://huggingface.co/datalab-to/chandra-ocr-2
+
 popd
