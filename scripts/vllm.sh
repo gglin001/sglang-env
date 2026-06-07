b8247fdc6db95d28833b38a9d85bbf083f59cdf4
Allen Guo <guosonglin001@gmail.com>
2026-06-07 15:08:20 +0800

    rename

--- scripts/vllm.sh
+++ scripts/vllm.sh
@@ -12,7 +12,7 @@ uv pip install --system vllm-0.22.1+cu129-cp38-abi3-manylinux_2_28_x86_64.whl
 # optional
 # uv pip install --system "transformers>=5.0.0"
 
-BASE_URL="https://wheels.vllm.ai/nightly/"
+# BASE_URL="https://wheels.vllm.ai/nightly/"
 # BASE_URL="https://wheels.vllm.ai/nightly/cu129/vllm"
 # BASE_URL="https://wheels.vllm.ai/nightly/cu130/vllm"
 
@@ -20,3 +20,5 @@ BASE_URL="https://wheels.vllm.ai/nightly/"
 # curl -O -L https://wheels.vllm.ai/fa27d4e9cf3c8d8a5a143f38c346b27c02b2c2e3/vllm-0.22.1rc1.dev237%2Bgfa27d4e9c-cp38-abi3-manylinux_2_28_x86_64.whl
 # cu129 eg:
 # curl -O -L https://wheels.vllm.ai/fa27d4e9cf3c8d8a5a143f38c346b27c02b2c2e3/vllm-0.22.1rc1.dev237%2Bgfa27d4e9c.cu129-cp38-abi3-manylinux_2_28_x86_64.whl
+
+for file in *%2B*; do mv -n -- "$file" "${file//%2B/+}"; done
