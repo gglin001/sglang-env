aa0a317fb6c6519ba11384e71438f8fbd41695c8
Allen Guo <guosonglin001@gmail.com>
2026-06-08 22:15:38 +0800

    rednote-hilab/dots.mocr

--- scripts/vllm-serve.sh
+++ scripts/vllm-serve.sh
@@ -6,29 +6,25 @@ export CUDA_VISIBLE_DEVICES="0"
 # export CUDA_VISIBLE_DEVICES="0,1"
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 
-# needs:
-# pixi shell -e cu130
-# pixi shell -e cu129
-
 export VLLM_NO_USAGE_STATS=1
 export VLLM_DO_NOT_TRACK=1
 export DO_NOT_TRACK=1
 
 args=(
   #
-  third_party/GLM-OCR
-  --served-model-name OCR
-  --speculative-config.method mtp
-  --speculative-config.num_speculative_tokens 1
+  # third_party/GLM-OCR
+  # --served-model-name zai-org/GLM-OCR
+  # --speculative-config.method mtp
+  # --speculative-config.num_speculative_tokens 1
   #
   # third_party/FireRed-OCR
-  # --served-model-name OCR
+  # --served-model-name FireRedTeam/FireRed-OCR-2B
   #
   # third_party/chandra-ocr-2
-  # --served-model-name OCR
+  # --served-model-name datalab-to/chandra-ocr-2
   #
-  # third_party/PaddleOCR-VL-1.6
-  # --served-model-name OCR
+  third_party/dots.mocr
+  --served-model-name rednote-hilab/dots.mocr
   #
   # --load-format dummy
   # --enforce-eager
@@ -44,4 +40,9 @@ args=(
   # -tp 2
   #
 )
-vllm serve "${args[@]}" 2>&1 | tee vllm.log
+pixi run -e cu129 vllm serve "${args[@]}" 2>&1 | tee vllm.log
+# pixi run -e cu130 vllm serve "${args[@]}" 2>&1 | tee vllm.log
+# needs:
+# pixi shell -e cu130
+# pixi shell -e cu129
+# vllm serve "${args[@]}" 2>&1 | tee vllm.log
