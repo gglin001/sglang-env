3ed011ed0b614fdcf445f1293e1a8dac2cd89908
Allen Guo <guosonglin001@gmail.com>
2026-06-10 00:02:51 +0800

    sglang-env

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -3,7 +3,7 @@ loginctl enable-linger $USER
 
 args=(
   #
-  --unit=vllm-serve
+  --unit=sglang-serve
   #
   -p "KillMode=control-group"
   -p "TimeoutStopSec=10"
@@ -12,17 +12,17 @@ args=(
   -p "Environment=VLLM_NO_USAGE_STATS=1"
   -p "WorkingDirectory=$PWD"
   #
-  scripts/vllm-serve.sh
+  scripts/sglang-serve.sh
   #
 )
 systemd-run --user "${args[@]}"
 
-systemctl --user stop vllm-serve
+systemctl --user stop sglang-serve
 
-systemctl --user status vllm-serve
-journalctl --user -u vllm-serve -f
-# journalctl --user -u vllm-serve -f -n 50
-systemctl --user show vllm-serve --property=MainPID,ControlGroup,Environment
+systemctl --user status sglang-serve
+journalctl --user -u sglang-serve -f
+# journalctl --user -u sglang-serve -f -n 50
+systemctl --user show sglang-serve --property=MainPID,ControlGroup,Environment
 systemd-cgtop --user
-# PID=$(systemctl --user show vllm-serve --property=MainPID --value)
+# PID=$(systemctl --user show sglang-serve --property=MainPID --value)
 # nsenter -t $PID -n -m bash
